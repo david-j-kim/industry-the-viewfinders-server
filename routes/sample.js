d8bdc4e2072673fd8cfa0ae213093cfa28aa5cbc
david-j-kim <david.jaekang.kim@gmail.com>
2022-05-13 13:30:49 -0400

    .

--- routes/sample.js
+++ routes/sample.js
@@ -2,29 +2,28 @@ const express = require("express");
 const router = express.Router();
 // const { readData, writeData, sampleData, sample2Data } = require('../util/readAndWrite');
 const sstk = require("shutterstock-api");
+sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
+
 
 // sample data
-router.get("/", (_req, res) => {
-    
-        sstk.setAccessToken(process.env.SHUTTERSTOCK_API_TOKEN);
+router.get("/:search", (req, res) => {
+        
+    const imagesApi = new sstk.ImagesApi();
 
-        const imagesApi = new sstk.ImagesApi();
+    const queryParams = {
+    "query": req.params.search,
+    "image_type": "photo",
+    "orientation": "vertical"
+    // "people_number": 3
+    };
 
-        const queryParams = {
-        "query": "flowers",
-        "image_type": "photo",
-        "orientation": "vertical"
-        // "people_number": 3
-        };
-
-        imagesApi.searchImages(queryParams)
-        .then((data) => {
-            res.status(200).json(data);
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    imagesApi.searchImages(queryParams)
+    .then((data) => {
+        res.status(200).json(data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
     
 });
 
